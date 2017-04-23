638fb4faa6078fae1f8341ea5a2cbc9c0eb26d00
David Lenkner <lenknerd@gmail.com>
2017-04-23 09:39:18 -0400

    More work on client a bit

--- Client/MediaSafe.rb
+++ Client/MediaSafe.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby -w
 # MediaSafeClient.rb
 # Client-side utility to examine directory contents, query backup server,
-# Parse results on whether files are already backed up, and scp them over
+# parse results on whether files are already backed up, and scp them over
 # Basically all the meat of the client is here
 #
 # David Lenkner, 2017
@@ -16,24 +16,26 @@ def cli_ops
 	cli_ops.separator ""
 	cli_ops.separator "Options:"
 	cli_ops.string "-s","-status", "File or directory to get status on"
+	cli_ops.string "-o","-output", "Where to save status (_MediaSafeStat.tsv)"
 	cli_ops.string "-r","-run", "MediaSafe status file to execute"
 
 	cli_ops
 end
 
-# Parse ARGV for those
+# Parse ARGV for those flags
 parser = Slop::Parser.new cli_ops
 begin
 	cli_args = parser.parse(ARGV).to_hash
 rescue Slop::UnknownOption
-	# Parsing hit unknown thing	
+	# Parsing hit unknown thing
+	puts 'Unknown argument to MediaSafe.rb!'
 	puts cli_ops
 	exit
 end
 
 # Now take actions based on arguments
-if(cli_args[:status] != nil)
-	puts "Getting status of: #{cli_args[:status]}"
+if(cli_args[:status] != nil)	
+	getAndRecordStatus(cli_args)
 elsif(cli_args[:run] != nil)
 	puts "Running actions defined in #{cli_args[:run]}"
 else
@@ -42,3 +44,18 @@ else
 	puts ''
 	puts cli_ops
 end
+
+# Function to get status of file/folder passed into -status option
+def getAndRecordStatus(opts)
+	if(opts[:output] == nil)
+		outputFileName = "_MediaSafeStat.tsv"
+	else
+		outputFileName = opts[:output]
+	end
+	puts "Getting status of: #{opts[:status]}"
+	puts "Writing to #{outputFileName}"
+end
+
+# Function to execute actions defined in status file
+
+
